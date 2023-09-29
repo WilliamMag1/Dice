4420a4afa041359b571b2933050bd57962dd0baa
willj <williamm534@nycstudents.net>
2023-09-29 18:14:39 -0400

    push

---
+++ dice.js
@@ -0,0 +1,26 @@
+function createDice(){
+    const dice = document.createElement("p");
+    const dice1 = document.createElement("p");
+    const total = document.createElement("p");
+
+    let x= Math.floor(Math.random()*6)+1;
+    let y= Math.floor(Math.random()*6)+1;
+    let z=x+y;
+
+    dice.textContent="Dice 1= "+ y;
+    dice1.textContent="Dice 2= " + x;
+    total.textContent = "The total is "+ z;
+
+
+    document.body.appendChild(dice);
+    document.body.appendChild(dice1);
+    document.body.appendChild(total);
+
+}
+
+const buttons = document.querySelectorAll('button');
+
+
+for(const button of buttons){
+    button.addEventListener("click",createDice);
+}
